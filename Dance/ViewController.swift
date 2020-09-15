02d3adb916162c8dccd55778d1a3e4bd1948e411
sealzhk <sealzhk@users.noreply.github.com>
2020-09-15 10:48:07 +0600

    Initial Commit

---
+++ Dance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dance
+//
+//  Created by Alua Zhakieva on 9/15/20.
+//  Copyright © 2020 Alua Zhakieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
